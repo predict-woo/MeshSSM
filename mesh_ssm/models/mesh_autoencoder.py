fix softmax

mesh_ssm/models/mesh_autoencoder.py
@@ -1,10 +1,11 @@
 from lightning.pytorch.utilities.types import OptimizerLRScheduler
+from torch.optim.optimizer import Optimizer
 from .mesh_encoder import MeshEncoder
 from .mesh_decoder import MeshDecoder
 from .residual_vector_quantize import ResidualVectorQuantizer
 import torch.nn as nn
 from pytorch3d.structures import Meshes
-from pytorch3d.io import load_obj
+from pytorch3d.io import save_obj
 from mesh_ssm.utils.mesh import FaceFeatureExtractor, Transforms
 from einops import rearrange
 import lightning as L
@@ -12,6 +13,22 @@ import torch
 import torch.nn.functional as F
 from mesh_ssm.utils.render import reconstruct_mesh, render_mesh
 import wandb
+from lightning.pytorch.utilities import grad_norm
+import numpy as np
+
+
+def getBack(var_grad_fn):
+    print(var_grad_fn)
+    for n in var_grad_fn.next_functions:
+        if n[0]:
+            try:
+                tensor = getattr(n[0], "variable")
+                print(n[0])
+                print("Tensor with grad found:", tensor)
+                print(" - gradient:", tensor.grad)
+                print()
+            except AttributeError as e:
+                getBack(n[0])
 
 
 class MeshAutoencoder(L.LightningModule):
@@ -22,9 +39,23 @@ class MeshAutoencoder(L.LightningModule):
         self.rq = ResidualVectorQuantizer(n_e=192, e_dim=192, num_quantizers=2)
         self.feature_extractor = FaceFeatureExtractor()
         self.transforms = None
+        self.criterion = nn.CrossEntropyLoss()
+
+    # def on_before_optimizer_step(self, optimizer: Optimizer) -> None:
+    #     enc_norms = grad_norm(self.encoder, norm_type=2)
+    #     dec_norms = grad_norm(self.decoder, norm_type=2)
+    #     rq_norms = grad_norm(self.rq, norm_type=2)
+    #     self.log_dict(enc_norms)
+    #     self.log_dict(dec_norms)
+    #     self.log_dict(rq_norms)
 
     def setup(self, stage=None):
         self.transforms = Transforms(device=self.device)
+        torch.set_printoptions(precision=4, sci_mode=False)
+
+    def test_step(self, batch: Meshes, batch_idx: int):
+        recons_loss, commit_loss = self.common_step(batch, batch_idx, render=True)
+        return recons_loss + commit_loss
 
     def training_step(self, batch: Meshes, batch_idx: int):
         recons_loss, commit_loss = self.common_step(batch, batch_idx)
@@ -36,7 +67,7 @@ class MeshAutoencoder(L.LightningModule):
         recons_loss, commit_loss = self.common_step(batch, batch_idx, render=True)
         self.log("validation reconstruction loss", recons_loss, batch_size=1)
         self.log("validation commitment loss", commit_loss, batch_size=1)
-        return recons_loss, commit_loss
+        return recons_loss + commit_loss
 
     def common_step(self, batch: Meshes, batch_idx: int, render=False):
         features = self.feature_extractor.get_derived_face_features(batch)
@@ -44,11 +75,12 @@ class MeshAutoencoder(L.LightningModule):
         face_features = self.encoder(features, edges)
 
         # split face features into vertex features
-        vertex_features = self.transforms.split_features(face_features)
+        vertex_features = rearrange(face_features, "f (n d) -> f n d", n=3)
 
         # average face features
         faces = batch.faces_packed()
         vertex_features = vertex_features.unsqueeze(0)  # [F, D] -> [1, F, D]
+
         vertex_features = self.transforms.avg_fv_feat(faces, vertex_features)
 
         # put vertex features into faces
@@ -73,46 +105,83 @@ class MeshAutoencoder(L.LightningModule):
         # render
         if render:
             reconstructed_mesh = reconstruct_mesh(recons)
-            image = render_mesh(reconstructed_mesh)
+            image, mesh = render_mesh(reconstructed_mesh)
 
             # log image to wandb
-            self.logger.experiment.log(
-                {"val_input_image": [wandb.Image(image, caption="val_input_image")]}
+            if hasattr(self.logger.experiment, "log"):
+                self.logger.experiment.log(
+                    {"val_input_image": [wandb.Image(image, caption="val_input_image")]}
+                )
+            save_obj(
+                f"results/mesh_{self.global_step}.obj",
+                mesh.verts_packed(),
+                mesh.faces_packed(),
             )
 
-        ## original loss
-        # verts = batch.verts_packed()
-        # mesh_one_hot_encoding = self.transforms.smooth_one_hot(verts)
-        # mesh_face_vertex_encoding = mesh_one_hot_encoding[faces]
-        # mesh_face_vertex_encoding = rearrange(
-        #     mesh_face_vertex_encoding, "f v x i -> f (v x) i"
-        # )
+        # original loss
+        verts = batch.verts_packed()
+        mesh_one_hot_encoding = self.transforms.smooth_one_hot(verts)
+        mesh_face_vertex_encoding = mesh_one_hot_encoding[faces]
+        mesh_face_vertex_encoding = rearrange(
+            mesh_face_vertex_encoding, "f v x i -> f (v x) i"
+        )
 
-        # log_probs = torch.log(recons + 1e-9)
-        # recons_loss = -torch.sum(log_probs * mesh_face_vertex_encoding)
+        # print(recons[0][3])
+        # print(mesh_face_vertex_encoding[0][3])
+        # print(rearrange(verts[faces], "f v x -> f (v x)")[0][3])
+        # print loss between the first
+
+        log_probs = torch.log(recons + 1e-9)
+        recons_loss = -torch.mean(log_probs * mesh_face_vertex_encoding)
+
+        test_log_probs = torch.log(mesh_face_vertex_encoding + 1e-9)
+        test_recons_loss = -torch.mean(test_log_probs * mesh_face_vertex_encoding)
+
+        # print(recons[0][0])
+        # print(mesh_face_vertex_encoding[0][0])
+        # # print(torch.sum(recons[0][0]))
+        # # print(torch.sum(mesh_face_vertex_encoding[0][0]))
+        # # print(rearrange(verts[faces], "f v x -> f (v x)")[0][0])
+        # print(recons_loss)
+        # print(test_recons_loss)
 
         ## l2 loss for each vertex
         # recons: [F,9,128]
         # choose maximum for each vertex coordinate: [F,9,128] -> [F,9]
-        recons = recons.max(dim=-1)[0]
-        # recons: [F,9]
-        # divide by 128
-        recons = recons / 128
-        # recons: [F,9]
 
-        # get face verts
-        verts = batch.verts_packed()
-        face_verts = verts[faces]
-        # face_verts: [F, 3, 3]
-        # flatten to [F,9]
-        face_verts = rearrange(face_verts, "f v c -> f (v c)")
-        # face_verts: [F, 9]
+        # recons = recons.max(dim=-1)[0]
+        # # recons: [F,9]
+        # # divide by 128
+        # recons = recons / 128
+        # # recons: [F,9]
 
-        # get l2 loss
-        recons_loss = torch.nn.functional.mse_loss(recons, face_verts)
+        # # get face verts
+        # verts = batch.verts_packed()
+        # face_verts = verts[faces]
+        # # face_verts: [F, 3, 3]
+        # # flatten to [F,9]
+        # face_verts = rearrange(face_verts, "f v c -> f (v c)")
+        # # face_verts: [F, 9]
+
+        # print(face_verts)
+
+        # # get l2 loss
+        # recons_loss = F.mse_loss(recons, face_verts)
+
+        ## Final loss
+        # verts = batch.verts_packed()
+        # face_verts = verts[faces]
+        # ground_truth = rearrange(face_verts, "f v c -> (f v c)").long() * 127
+        # recons = rearrange(recons, "f v c -> (f v) c")
+
+        # torch.set_printoptions(threshold=torch.inf)
+        # print(ground_truth[:9], ground_truth.shape)
+        # print(recons[:9], recons.shape)
+
+        # recons_loss = self.criterion(recons, ground_truth)
 
         return recons_loss, commit_loss
 
     def configure_optimizers(self) -> OptimizerLRScheduler:
-        optimizer = torch.optim.Adam(self.parameters(), lr=0.0001)
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
         return optimizer

mesh_ssm/models/mesh_decoder.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 import torch.nn.functional as F
+from einops import rearrange
 
 
 def conv3(in_planes, out_planes, stride=1):
@@ -86,7 +87,14 @@ class ResNet(nn.Module):
         x = self.layer4(x)
         x = torch.permute(x, (0, 2, 1))
         logits = self.fc(x)
-        probas = F.softmax(logits, dim=1)
+
+        # logits = F.softplus(logits)
+        # logits = logits + 1e-9
+        # logits = logits / torch.sum(logits, dim=-1, keepdim=True)
+        logits = rearrange(logits, "b f (fc p) -> b f fc p", fc=9)
+        probas = F.softmax(logits, dim=-1)
+        probas = rearrange(probas, "b f fc p -> b f (fc p)")
+        # return probas
         return probas
 
 

mesh_ssm/models/mesh_encoder.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from torch_geometric.nn import SAGEConv
+from torch_geometric.nn import SAGEConv, BatchNorm
 from torch_geometric.data import Data
 from mesh_ssm.utils.mesh import FaceFeatureExtractor
 

mesh_ssm/utils/mesh.py
@@ -39,20 +39,18 @@ class FaceFeatureExtractor:
         angles = derive_angle(diff_1, diff_2)
 
         # normals
-        cross = torch.cross(diff_1[:, 0, :], diff_2[:, 0, :], dim=-1)
-        normals = F.normalize(cross, dim=-1, p=2)
+        normals = meshes.faces_normals_packed()
 
-        # area
-        area = 0.5 * torch.unsqueeze(torch.norm(cross, dim=-1), -1)
+        # areas
+        area = meshes.faces_areas_packed().unsqueeze(-1)
 
-        # concat each face features into 9 vertice coordinates, 1 area, 3 angles, 3 normals -> 16 features
-        # [B, 3, 3] -> [B*3, 3]
-        face_verts = rearrange(face_verts, "b n v -> (b n) v")
-        face_verts = self.positional_encoder.encode(face_verts)
-        face_verts = rearrange(face_verts, "(b n) v -> b (n v)", n=3)
+        # positional encoding for vertices
+        enc_verts = self.positional_encoder.encode(verts)
+        enc_face_vert = enc_verts[faces]
+        enc_face_vert = rearrange(enc_face_vert, "f v d -> f (v d)")
 
         # concat face features
-        face_features = torch.cat([face_verts, area, angles, normals], dim=-1)
+        face_features = torch.cat([enc_face_vert, area, angles, normals], dim=-1)
 
         return face_features
 
@@ -193,6 +191,9 @@ class Transforms:
         one_hot_verts_reshaped = one_hot_verts.view(-1, 1, 128)
         one_hot_verts_smoothed = self.conv1d(one_hot_verts_reshaped)
         one_hot_verts_smoothed = one_hot_verts_smoothed.view(-1, 3, 128)
+        one_hot_verts_smoothed = one_hot_verts_smoothed / one_hot_verts_smoothed.sum(
+            dim=-1, keepdim=True
+        )
 
         return one_hot_verts_smoothed
 

train_autoencoder.py
@@ -3,22 +3,36 @@ from pytorch3d.io import load_obj
 from mesh_ssm.utils.mesh import FaceFeatureExtractor
 from pytorch3d.structures import Meshes
 from mesh_ssm.utils.augment import augment_mesh, normalize_mesh
-from data import AugmentedHorseMeshDataset, collate_meshes
 from torch.utils.data import DataLoader
 import lightning as L
 from lightning.pytorch.loggers import WandbLogger
+from torch.utils.data import Dataset, DataLoader
 
-TEST = False
 
-if TEST:
-    verts, faces, aux = load_obj("Horse.obj", load_textures=False)
+class AugmentedHorseMeshDataset(Dataset):
+    def __init__(self, obj_path, num_samples):
+        self.obj_path = obj_path
+        self.num_samples = num_samples
+        self.verts, self.faces, _ = load_obj(obj_path)
+        self.mesh = Meshes(verts=[self.verts], faces=[self.faces.verts_idx])
 
-    meshes = Meshes(verts=[verts], faces=[faces.verts_idx])
-    meshes = normalize_mesh(meshes)
-    autoencoder = MeshAutoencoder()
-    autoencoder.setup()
-    autoencoder.training_step(meshes, 0)
-    exit()
+    def __len__(self):
+        return self.num_samples
+
+    def __getitem__(self, idx):
+        augmented_mesh = augment_mesh(self.mesh)
+        # augmented_mesh = self.mesh
+        return augmented_mesh
+
+
+def collate_meshes(batch):
+    verts = []
+    faces = []
+    for mesh in batch:
+        verts.append(mesh.verts_packed())
+        faces.append(mesh.faces_packed())
+    return Meshes(verts=verts, faces=faces)
+
 
 wandb_logger = WandbLogger(log_model="all", project="mesh-ssm")
 
@@ -31,12 +45,12 @@ trainer = L.Trainer(
 )
 model = MeshAutoencoder()
 
-train_dataset = AugmentedHorseMeshDataset("cone.obj", 100)
+train_dataset = AugmentedHorseMeshDataset("Horse.obj", 6400)
 train_dataloader = DataLoader(
-    train_dataset, batch_size=1, collate_fn=collate_meshes, num_workers=10
+    train_dataset, batch_size=64, collate_fn=collate_meshes, num_workers=30
 )
 
-val_dataset = AugmentedHorseMeshDataset("cone.obj", 1)
+val_dataset = AugmentedHorseMeshDataset("Horse.obj", 1)
 val_dataloader = DataLoader(val_dataset, batch_size=1, collate_fn=collate_meshes)
 
 